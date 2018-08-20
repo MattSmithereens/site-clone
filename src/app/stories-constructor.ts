6327b9030df5dbad79fa89e151e48da5a97fb716
Matt Smith <mattsmithereens@gmail.com>
2018-08-19 20:33:11 -0700

    add stories.constructor.ts

---
+++ src/app/stories-constructor.ts
@@ -0,0 +1,11 @@
+export class Story {
+    id: string;
+    name: string;
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    publishedAt: Date;
+  }
+  
