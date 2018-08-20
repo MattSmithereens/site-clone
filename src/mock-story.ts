d0aefc27cae65fbd6da9032866c4fb563bb6d2ee
Matt Smith <mattsmithereens@gmail.com>
2018-08-19 21:21:57 -0700

    add additional styling and non-functional stories constructor

---
+++ src/mock-story.ts
@@ -0,0 +1,12 @@
+import { Story } from './story'
+
+export let STORIES: Story[] = [
+  {id: 'vice-news', 
+  name: 'Vice News', 
+  author: 'Emma Ockerman', 
+  title: 'New Haven K2 overdoses climb to over 100: “I’m afraid it’s going to mess people up”', 
+  description: 'People are still \"dropping\" by the dozens in one New Haven green.', 
+  url: 'https://news.vice.com/en_us/article/43p8xp/new-haven-k2-overdoses-climb-to-over-100-im-afraid-its-going-to-mess-people-up', 
+  urlToImage: 'https://video-images.vice.com/articles/5b7712c4557a590006f5d1d5/lede/1534530245734-new-haven-k2.jpeg?crop=1xw:0.7888970051132214xh;center,center'} //, 
+  //publishedAt: '2018-08-17T18:53:55.0610754+00:00'}
+]
---
+++ src/story.ts
@@ -0,0 +1,12 @@
+import { Url } from "url";
+
+export class Story {
+    id: string;
+    name: string;
+    author: string;
+    title: string;
+    description: string;
+    url: string;
+    urlToImage: string;
+    //publishedAt: DateTime;
+  }
